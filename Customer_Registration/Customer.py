c10621b12d7150c03bb7725d334a01ec813a49ef
Daxi18K <shrimali.tdauksh@gmail.com>
2023-04-22 11:34:08 +1000

    Customer Registration tables built

--- Customer_Registration/Customer.py
+++ Customer_Registration/Customer.py
@@ -47,11 +47,133 @@ class Registration(Base):
     
     __table_args__ = {"schema":"Customer","extend_existing": True}
 
-        # Get a number from the sequence and retain it for the registration purpose.
-        # But this number should only go for new registration.
-        # Any modification to existing customer or their respective details will be done via calling individual child class.
         
         
+        
+class customerFinancials:
+    
+    def __init__(self,
+                 recordId: int,
+                 customerId: int,
+                 expectedFundSize: float,
+                 investmentProportion: float,
+                 frequencyOfInvestment: str,
+                 dateUpdated, DateTime):
+        
+        assert recordId >= 0.0
+        assert customerId >= 0.0
+        assert expectedFundSize >= 0
+        assert investmentProportion >= 0.0
+        assert frequencyOfInvestment != ''
+        assert dateUpdated != ''
+        
+        self.recordID = recordId
+        self.customerId = customerId
+        self.expectedFundSize = expectedFundSize
+        self.investmentProportion = investmentProportion
+        self.frequencyOfInvestment = frequencyOfInvestment
+        self.dateUpdated = dateUpdated
+        
+    __tablename__ = "CustomerFinancials"
+        
+    recordId = Column("Record_ID", Numeric, primary_key=True)
+    customeId = Column("Customer_ID", Numeric)
+    expectedFundSize = Column("Expected_Fund_Size", Numeric)
+    investmentProportion = Column("Investment_Prop",Float)
+    frequencyOfInvestment = Column("Frequency_of_Investment", String)
+    dateUpdated = Column("Date_Updated",DateTime)
+        
+    __table_args__ = {"schema":"Customer","extend_existing": True}
+    
+
+    
+class customerPortfolioPreference:
+    
+    def __init__(self,
+                 recordId: int,
+                 customerId: int,
+                 portfolioDriver: String,
+                 riskProfile: String,
+                 returnProfile: String,
+                 dateUpdated: DateTime):
+        
+        assert recordId >= 0.0
+        assert customerId >= 0.0
+        assert portfolioDriver != ''
+        assert riskProfile != ''
+        assert returnProfile != ''
+        assert dateUpdated != ''
+        
+        self.recordId = recordId
+        self.customerId = customerId
+        self.portfolioDriver = portfolioDriver
+        self.riskProfile = riskProfile
+        self.returnProfile = returnProfile
+        self.dateUpdated = dateUpdated
+        
+    __tablename__ = "CustomerPortfolioPreference"
+    
+    recordId = Column("Record_ID", Numeric, primary_key=True)
+    customeId = Column("Customer_ID", Numeric)
+    portfolioDriver = Column("Portfolio_Driver", String)
+    riskProfile = Column("Risk_Profile",String)
+    returnProfile = Column("Return_Profile", String)
+    dateUpdated = Column("Date_Updated",DateTime)
+    
+    __table_args__ = {"schema":"Customer","extend_existing": True}
+    
+class customerStrategyPreference:
+    
+    def __init__(self,
+                 recordId: int,
+                 customerId: int,
+                 strategyId: int,
+                 dateUpdated: DateTime):
+        
+        assert recordId >= 0.0
+        assert customerId >= 0.0
+        assert strategyId >= 0
+        assert dateUpdated != ''
+        
+        self.recordId = recordId
+        self.customerId = customerId
+        self.strategyId = strategyId
+        self.dateUpdated = dateUpdated
+        
+    __tablename__ = "CustomerStrategyPreference"
+    
+    recordId = Column("Record_ID", Numeric, primary_key=True)
+    customeId = Column("Customer_ID", Numeric)
+    strategyId = Column("Strategy_ID", Integer)
+    dateUpdated = Column("Date_Updated",DateTime)
+    
+    __table_args__ = {"schema":"Customer","extend_existing": True}
+    
+
+class customerStockPreference:
+    
+    def __init__(self,
+                 recordId: int,
+                 customerId: int,
+                 etfName: String,
+                 portfolioCategory: String,
+                 dateUpdated: DateTime):
+        
+        assert recordId >= 0.0
+        assert customerId >= 0.0
+        assert etfName != ''
+        assert portfolioCategory != ''
+        assert dateUpdated != ''
+        
+    __tablename__ = 'CustomerStockPreference'
+    
+    recordId = Column("Record_ID", Numeric, primary_key=True)
+    customeId = Column("Customer_ID", Numeric)
+    etfName = Column("ETF_Name",String)
+    portfolioCategory = Column("Portfolio_Category", String)
+    dateUpdated = Column("Date_Updated",DateTime)
+        
+    __table_args__ = {"schema":"Customer","extend_existing": True}
         
         
 ## Ideas for the input required for customer registration
--- main.py
+++ main.py
@@ -17,14 +17,19 @@ print(date_time)
 
 sys.path.insert(0, r'C:\Users\dashrimali\Quant Risk Management Course\QuantPortfolioRisk-Attempt-1')
 
+
+## Creating Sample instances for Registration
 cust1 = Registration(recordId=112233,customerId=123123, dob=date(1992,3,18),state='NSW',occupation='Consultant',investmentExperience=4,dateUpdated=date_time)
 session.add(cust1)
-
 cust2 = Registration(recordId=223344,customerId=123123, dob=date(1994,4,18),state='QLD',occupation='Engineer',investmentExperience=1,dateUpdated=date_time)
 session.add(cust2)
-
 cust3 = Registration(recordId=334455,customerId=123123, dob=date(1993,1,22),state='SA',occupation='Financial Advisor',investmentExperience=10,dateUpdated=date_time)
 session.add(cust3)
+
+
+
+
+
 
 try:
     session.commit()
