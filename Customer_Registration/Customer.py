6c05920ff73a70c0bb37be98984bfd87e5fa5ae3
Daxi18K <shrimali.tdauksh@gmail.com>
2023-04-30 14:02:15 +1000

    New entry process Test 1

--- Customer_Registration/Customer.py
+++ Customer_Registration/Customer.py
@@ -3,7 +3,19 @@ import sys
 # Insert the path of modules folder 
 sys.path.insert(0, r'C:\Users\dashrimali\Quant Risk Management Course\QuantPortfolioRisk-Attempt-1')
 
-from Utility.Utility import Base, create_engine, PrimaryKeyConstraint, ForeignKey, Column, String, Integer, CHAR, Numeric, Float, Date, DateTime
+from Utility.Utility import (Base,
+                            PrimaryKeyConstraint,
+                            ForeignKey,
+                            Column,
+                            String,
+                            Integer,
+                            CHAR,
+                            Numeric,
+                            Float,
+                            Date,
+                            DateTime,
+                            relationship                            
+)
 
 class Registration(Base):
         
@@ -11,173 +23,105 @@ class Registration(Base):
                  recordId: int,
                  customerId: int,
                  dob: Date,
-                 state: str,
-                 occupation: str,
+                 stateId: int,
+                 occupationId: int,
                  investmentExperience: int,
-                 dateUpdated: DateTime
-                 ):
-        
-        assert recordId >= 0.0
-        assert customerId >= 0.0
-        assert dob != ''
-        assert state != ''
-        assert occupation != ''
-        assert investmentExperience >= 0
-        assert dateUpdated != ''
-        
-        self.recordId = recordId
-        self.customeId = customerId
-        self.dob = dob
-        self.state = state
-        self.occupation = occupation
-        self.investmentExperience = investmentExperience
-        self.dateUpdated = dateUpdated
-    
-    
-    __tablename__ = "CustomerMaster"
-    
-        
-    recordId = Column("Record_ID", Numeric, primary_key=True)
-    customeId = Column("Customer_ID", Numeric)
-    dob = Column("Date_of_Birth", Date)
-    state = Column("State", String)
-    occupation = Column("Occupation", String)
-    investmentExperience = Column("Investment_Exp",String)
-    dateUpdated = Column("Date_Updated", DateTime)
-    
-    __table_args__ = {"schema":"Customer","extend_existing": True}
-
-        
-        
-        
-class customerFinancials:
-    
-    def __init__(self,
-                 recordId: int,
-                 customerId: int,
-                 expectedFundSize: float,
+                 expectedFundSize: Numeric,
                  investmentProportion: float,
-                 frequencyOfInvestment: str,
-                 dateUpdated: DateTime
-                 ):
-        
-        assert recordId >= 0.0
-        assert customerId >= 0.0
-        assert expectedFundSize >= 0
-        assert investmentProportion >= 0.0
-        assert frequencyOfInvestment != ''
-        assert dateUpdated != ''
-        
-        self.recordID = recordId
-        self.customerId = customerId
-        self.expectedFundSize = expectedFundSize
-        self.investmentProportion = investmentProportion
-        self.frequencyOfInvestment = frequencyOfInvestment
-        self.dateUpdated = dateUpdated
-        
-    __tablename__ = "CustomerFinancials"
-        
-    recordId = Column("Record_ID", Numeric, primary_key=True)
-    customeId = Column("Customer_ID", Numeric)
-    expectedFundSize = Column("Expected_Fund_Size", Numeric)
-    investmentProportion = Column("Investment_Prop",Float)
-    frequencyOfInvestment = Column("Frequency_of_Investment", String)
-    dateUpdated = Column("Date_Updated",DateTime)
-        
-    __table_args__ = {"schema":"Customer","extend_existing": True}
-    
-
-    
-class customerPortfolioPreference:
-    
-    def __init__(self,
-                 recordId: int,
-                 customerId: int,
+                 frequencyOfInvestmentId: int,
                  portfolioDriver: String,
-                 riskProfile: String,
-                 returnProfile: String,
-                 dateUpdated: DateTime
-                 ):
-        
-        assert recordId >= 0.0
-        assert customerId >= 0.0
-        assert portfolioDriver != ''
-        assert riskProfile != ''
-        assert returnProfile != ''
-        assert dateUpdated != ''
-        
-        self.recordId = recordId
-        self.customerId = customerId
-        self.portfolioDriver = portfolioDriver
-        self.riskProfile = riskProfile
-        self.returnProfile = returnProfile
-        self.dateUpdated = dateUpdated
-        
-    __tablename__ = "CustomerPortfolioPreference"
-    
-    recordId = Column("Record_ID", Numeric, primary_key=True)
-    customeId = Column("Customer_ID", Numeric)
-    portfolioDriver = Column("Portfolio_Driver", String)
-    riskProfile = Column("Risk_Profile",String)
-    returnProfile = Column("Return_Profile", String)
-    dateUpdated = Column("Date_Updated",DateTime)
-    
-    __table_args__ = {"schema":"Customer","extend_existing": True}
-    
-class customerStrategyPreference:
-    
-    def __init__(self,
-                 recordId: int,
-                 customerId: int,
+                 riskProfileId: int,
+                 returnProfileId: int,
                  strategyId: int,
                  dateUpdated: DateTime
                  ):
         
         assert recordId >= 0.0
         assert customerId >= 0.0
+        assert dob != ''
+        assert stateId >= 0
+        assert occupationId >= 0
+        assert investmentExperience >= 0
+        assert expectedFundSize >= 0
+        assert investmentProportion >= 0
+        assert frequencyOfInvestmentId >= 0
+        assert portfolioDriver != ''
+        assert riskProfileId >= 0
+        assert returnProfileId >= 0
         assert strategyId >= 0
         assert dateUpdated != ''
         
         self.recordId = recordId
-        self.customerId = customerId
+        self.customeId = customerId
+        self.dob = dob
+        self.stateId = stateId
+        self.occupationId = occupationId
+        self.investmentExperience = investmentExperience
+        self.expectedFundSize = expectedFundSize
+        self.investmentProportion = investmentProportion
+        self.frequencyOfInvestmentId = frequencyOfInvestmentId
+        self.portfolioDriver = portfolioDriver
+        self.riskProfileId = riskProfileId
+        self.returnProfileId = returnProfileId
         self.strategyId = strategyId
         self.dateUpdated = dateUpdated
-        
-    __tablename__ = "CustomerStrategyPreference"
     
+    
+    __tablename__ = "FactCustomerDetails"
+    
+        
     recordId = Column("Record_ID", Numeric, primary_key=True)
     customeId = Column("Customer_ID", Numeric)
-    strategyId = Column("Strategy_ID", Integer)
-    dateUpdated = Column("Date_Updated",DateTime)
+    dob = Column("Date_of_Birth", Date)
+    stateId = Column(Integer, ForeignKey('DimStateDetails.State_ID'))
+    stateRelationship = relationship('DimStateDetails', backref='stateRelationship')
+    occupationId = Column(Integer, ForeignKey('DimOccupationDetails.Occupation_ID'))
+    occupation = relationship('DimOccupationDetails', backref = 'occupation_name')
+    investmentExperience = Column("Investment_Exp",Integer)
+    expectedFundSize = Column("Expected_Fund_Size", Numeric)
+    investmentProportion = Column("Residual_Proportion", Numeric)
+    frequencyOfInvestmentId = Column(Integer, ForeignKey('DimInvestmentFreqDetails.Frequency_of_Investment_ID'))
+    frequencyOfInvestment = relationship('DimInvestmentFreqDetails', backref='freq_of_investment')
+    portfolioDriver = Column("Portfolio_Driver", String)
+    riskProfileId = Column(Integer, ForeignKey('DimRiskProfileDetails.Risk_Profile_ID'))
+    riskProfile = relationship('DimRiskProfileDetails', backref='risk_profile')
+    returnProfileId = Column(Integer, ForeignKey('DimReturnProfileDetails.Return_Profile_ID'))
+    returnProfile = relationship('DimReturnProfileDetails', backref='return_profile')
+    strategyId = Column(Integer, ForeignKey('DimStrategyDetails.Strategy_ID'))
+    strategy = relationship('DimStrategyDetails', backref='strategy_name')
+    dateUpdated = Column("Date_Updated", DateTime)
     
     __table_args__ = {"schema":"Customer","extend_existing": True}
+
+        
+
+
+class StateDetails(Base):
+        
+    def __init__(self,
+                 stateId: int,
+                 state: str
+                 ):
+                
+        self.stateId = stateId
+        self.state = state
+    
+    __tablename__ = "DimStateDetails"
     
 
-class customerStockPreference:
+    stateId = Column(Integer, ForeignKey('DimStateDetails.State_ID'))
+    state = Column("State",String)
+    stateRelationship = relationship('FactCustomerDetails', backref='stateRelationship')
     
-    def __init__(self,
-                 recordId: int,
-                 customerId: int,
-                 etfName: String,
-                 portfolioCategory: String,
-                 dateUpdated: DateTime
-                 ):
-        
-        assert recordId >= 0.0
-        assert customerId >= 0.0
-        assert etfName != ''
-        assert portfolioCategory != ''
-        assert dateUpdated != ''
-        
-    __tablename__ = 'CustomerStockPreference'
-    
-    recordId = Column("Record_ID", Numeric, primary_key=True)
-    customeId = Column("Customer_ID", Numeric)
-    etfName = Column("ETF_Name",String)
-    portfolioCategory = Column("Portfolio_Category", String)
-    dateUpdated = Column("Date_Updated",DateTime)
-        
     __table_args__ = {"schema":"Customer","extend_existing": True}
+
+          
+        
+
+    
+
+    
+
         
         
 ## Ideas for the input required for customer registration
